33aaaeb19f97303e6327fca1d6fab227ffd8ab7d
karishma upreti <113200296+karishmaupreti@users.noreply.github.com>
2024-07-16 21:49:39 +0530

    Create Permute two arrays such that sum of every pair is greater or equal to K.cpp

---
+++ Permute two arrays such that sum of every pair is greater or equal to K.cpp
@@ -0,0 +1,17 @@
+class Solution {
+ public:
+    bool isPossible(long long a[], long long b[], int n, long long k) {
+        sort(a,a+n);
+        sort(b,b+n);
+       int i=0,j=n-1;
+       while(i<n && j>=0){
+           
+           if(a[i]+b[j]<k) return false;
+           else{
+               i++;
+               j--;
+           }
+       }
+       return true;
+    }
+};
