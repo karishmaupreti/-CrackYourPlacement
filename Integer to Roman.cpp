da513020bbb33ceccbabe6a322c453026a121b48
karishma upreti <113200296+karishmaupreti@users.noreply.github.com>
2024-07-18 03:15:13 +0530

    Create Integer to Roman.cpp

---
+++ Integer to Roman.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<pair<int,string>>v={
+            {1,"I"},{4,"IV"},{5,"V"},{9,"IX"},{10,"X"},{40,"XL"},{50,"L"},{90,"XC"},{100,"C"},{400,"CD"},{500,"D"},{900,"CM"},{1000,"M"}
+        };
+
+
+    string ans="";
+       for(int i=v.size()-1;i>=0;i--){
+        while(num>=v[i].first){
+            ans+=v[i].second;
+            num=num-v[i].first;
+        }
+       }
+       return ans;
+    }
+};
